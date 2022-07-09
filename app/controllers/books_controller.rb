664a906ea99294e5bba1b49e03ad17cb7b67c40f
misaki0000 <108600708+misaki0000@users.noreply.github.com>
2022-07-09 15:01:55 +0000

    [更新機能は出来ていない、削除機能追加]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,6 +28,13 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
 
